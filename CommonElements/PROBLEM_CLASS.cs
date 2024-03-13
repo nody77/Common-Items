6b28849b15b99d1921a064ccc1cd6af38fc187ae
Nada <104033293+nody77@users.noreply.github.com>
2024-03-13 10:17:37 +0200

    dividing two arrays and searching in them

--- CommonElements/PROBLEM_CLASS.cs
+++ CommonElements/PROBLEM_CLASS.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,10 +30,51 @@ namespace Problem
         static public int[] RequiredFuntion(int[] arr1, int[] arr2)
         {
             //REMOVE THIS LINE BEFORE START CODING
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (arr1.Length == 0 || arr2.Length == 0) 
+            {
+                return new int[0];
+            }
+            else if (arr1.Length == 1 && arr2.Length == 1) 
+            {
+                if (arr1[0] == arr2[0])
+                {
+                    return new int[] { arr1[0] };
+                }
+                else
+                {
+                    return new int[0];
+                }
+
+            }
+            else
+            {
+                int mid1 = arr1.Length / 2;
+                int mid2 = arr2.Length / 2;
+
+                int[] left1 = new int[mid1];
+                int[] right1 = new int[arr1.Length - mid1];
+                Array.Copy(arr1, 0, left1, 0, mid1);
+                Array.Copy(arr1, mid1, right1, 0, arr1.Length - mid1);
+
+                int[] left2 = new int[mid2];
+                int[] right2 = new int[arr2.Length - mid2];
+                Array.Copy(arr2, 0, left2, 0, mid2);
+                Array.Copy(arr2, mid2, right2, 0, arr2.Length - mid2);
+
+                int[] commonLeft = RequiredFuntion(left1, left2);
+                int[] commonRight = RequiredFuntion(right1, right2);
+                int[] commonLeftandRight1 = RequiredFuntion(left1, right2);
+                int[] commonLeftandRight2 = RequiredFuntion(left2, right1);
+
+                int [] merge = commonLeftandRight1.Concat(commonLeftandRight2).ToArray();
+                int[] merge2 = commonLeft.Concat(commonRight).ToArray();
+
+                return merge.Concat(merge2).ToArray();
+
+            }
         }
         
-
         #endregion
     }
 }
